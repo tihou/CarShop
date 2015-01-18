09c73bd38ded7d7f8fe5faafc90528af8a10f78f
LiMingCH <379454125@qq.com>
2015-01-18 23:02:59 +0800

    拆分出远程服务客户端服务端

--- BaseService/com/liming/service/LoginAuthoriedService.java
+++
@@ -1,9 +0,0 @@
-package com.liming.service;
-
-import java.util.List;
-
-public interface LoginAuthoriedService {
-	public String checkUserExist(String userid);
-	public String checkLoginValid(String userid,String password);
-	public List getMember();
-}
